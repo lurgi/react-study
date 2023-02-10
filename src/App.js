ff27dac267e2bb312317cb4b39a8f187f452e83c
lurgi <wahoo9040@naver.com>
2023-02-11 08:05:18 +0900

    20230211

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import { useEffect, useState } from "react";
 import Button from "./Button";
 
 function App() {
-  const [count,setCount] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const countUp = () => {setCount((prop)=>prop+1)};
-  const onChange = (event) =>{
-    setKeyword(event.target.value);
+  const [toDo, setToDO] = useState("");
+  const onChange = (event) => {
+    setToDO(event.target.value)
   }
-  // console.log("SEARCH FOR", keyword)
-  const a = () => console.log("SEARCH FOR", keyword)
-  useEffect(a,[keyword])
   return (
     <div>
-      <input onChange={onChange} value={keyword} type="text" placeholder ="Search here..."></input>
-      {count}
-      <button onClick={countUp}>HI</button>
+      <input onChange={onChange} type="text" placeholder="Write"/>
     </div>
   );
 }
